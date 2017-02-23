eda0ab4ffca53a43ec570001cc82a8e24eade88b
YS-PC\shunnta <yabu.soccer@outlook.jp>
2017-02-23 14:23:00 +0900

    punの導入

---
+++ electlium/Assets/Resourse/Script/NetworkData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkData : Photon.MonoBehaviour{
+
+
+    public string spriteName = "";
+
+    void Start()
+    {
+      
+    }
+
+	public void Init(string name){
+		this.name = name;
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+        if (stream.isWriting)
+            {                
+                stream.SendNext(spriteName);
+              
+            }
+            else
+            {
+                spriteName = (string)stream.ReceiveNext();          
+            }
+     }
+    
+}
